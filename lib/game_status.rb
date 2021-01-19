65c5569cf8b11c3ac9b240d76e5b42a2aae8cd44
gdilthey <george.dilthey@gmail.com>
2021-01-19 23:52:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,5 +12,21 @@ WIN_COMBINATIONS = [
   [1,4,7],
   [2,5,8],
   [0,4,8],
-  [2,4,6] 
+  [2,4,6]
 ]
+
+def won(board)
+  xpos = []
+  opos = []
+  board.each{|pos|
+  if position_taken(board,pos)
+    pos == 'X' ? xpos << pos : opos << pos;
+  end
+  }
+  WIN_COMBINATIONS.each{|comb|
+    if comb.all?{|pos| xpos.include? || opos.include?}
+      return comb
+    else
+      return false
+  }
+end
