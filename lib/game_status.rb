595e0a1de85ea42a227064dac5ded8f42b669d37
gdilthey <george.dilthey@gmail.com>
2021-01-20 02:29:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.any?{|comb|
     if comb.all?{|pos| position_taken?(board, pos)}
-      arr = comb.each{|pos| board[pos]}
+      arr = comb.each{|pos| return board[pos]}
       puts arr
       if arr.uniq.size <=1
         return comb
